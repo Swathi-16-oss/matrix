bb89bc8de85e912c80fb01eaa7f3f5d6f9aaacab
Devarasetty Swathi <70877614+Swathi-16-oss@users.noreply.github.com>
2020-10-15 23:58:04 +0530

    Create print matrix.py

---
+++ print matrix.py
@@ -0,0 +1,24 @@
+
+m=int(input("enter m order"))
+n=int(input("enter n order"))
+matrix1=[]
+for i in range(m):#for row
+    a=[]
+    for j in range(n):#for column
+        a.append(int(input("enter [{}{}] element".format(i,j))))
+    matrix1.append(a)#to add elements to matrix
+#for printing first matrix
+for i in range(m):
+    for j in range(n):
+        print([matrix1[i][j]],end=" ")
+    print()
+ 
+ '''o/p:
+ enter m order2
+enter n order2
+enter [00] element12
+enter [01] element3
+enter [10] element4
+enter [11] element23
+[12] [3] 
+[4] [23] '''
